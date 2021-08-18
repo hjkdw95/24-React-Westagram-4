53217ba1ae9e503c0dbc0dc473c5c41fac194470
Casey <hjkdw9506@gmail.com>
2021-08-18 16:52:06 +0900

    🩹  push test

--- weestagram-project/src/index.js
+++ weestagram-project/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Routes from "./routes";
+
+import "./index.scss";
+import "./styles/common.scss";
+import "./styles/reset.scss";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
